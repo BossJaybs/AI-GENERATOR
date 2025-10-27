6ff15b6d76651537384ecd047888ff90ababc028
BossJaybs <masterbuten2020@gmail.com>
2025-10-27 14:44:57 +0800

    Fix TypeScript error by adding type annotation to prompt parameter in sendWithRetry function

--- utils/AiModel.tsx
+++ utils/AiModel.tsx
@@ -37,7 +37,7 @@ function isRetryableError(err: any) {
   return /(429|500|502|503|504)/.test(message) || /fetch|network|timeout/i.test(message);
 }
 
-export async function sendWithRetry(prompt, options = {}) {
+export async function sendWithRetry(prompt: string, options = {}) {
   const {
     maxRetries = 3,
     baseDelayMs = 750,
