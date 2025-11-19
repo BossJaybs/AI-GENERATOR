ee7f6e547c01c09e77501a7ae9fed4916b95035f
BossJaybs <masterbuten2020@gmail.com>
2025-11-19 23:36:38 +0800

    Fix client-side issues in OutputSection.tsx

--- app/dashboard/content/_component/OutputSection.tsx
+++ app/dashboard/content/_component/OutputSection.tsx
@@ -1,5 +1,7 @@
+'use client'
 import React from 'react'
 import MDEditor from '@uiw/react-md-editor';
+import '@uiw/react-md-editor/markdown-editor.css';
 import { Copy } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 
